3888a97356787b05a0f02ecacd70f0afeee86623
morozgit <69896562+morozgit@users.noreply.github.com>
2023-04-17 21:52:00 +0300

    fix bug

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import requests
 
 
-def ru_letter(text, alphabet=set('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')):
+def chek_ru_letter(text, alphabet=set('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')):
     return not alphabet.isdisjoint(text.lower())
 
 
 def show_weather(places):
     for place in places:
-        if ru_letter(place):
-            url = 'http://wttr.in/{}?nqTmM&lang=ru'.format(place)
+        if chek_ru_letter(place):
+            url = 'http://wttr.in/{}?nqTM&lang=ru'.format(place)
         else:
             url = 'http://wttr.in/{}?nTqu&lang=en'.format(place)
-    response = requests.get(url)
-    response.raise_for_status()
-    print(response.text)
+        response = requests.get(url)
+        response.raise_for_status()
+        print(response.text)
 
 
 def main():
